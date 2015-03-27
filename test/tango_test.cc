759d5a8b2bd59238498e9000fd91af77b9a480f0
Nicholas Hannah <nchlshnnh@gmail.com>
2015-03-27 15:40:05 +1100

    Do a proper (big) send receive test between two grids.

--- test/tango_test.cc
+++ test/tango_test.cc
@@ -48,6 +48,50 @@ TEST(Tango, send_receive)
     tango_finalize();
 }
 
+/* Do a big field send/receive between two differently sized grids. */
+TEST(Tango, big_send_receive)
+{
+    int rank;
+    int src_x = 192, src_y = 94;
+    double src_u[src_x * src_y];
+
+    for (int i = 0; i < src_x * src_y; i++) {
+        src_u[i] = 7;
+    }
+
+    string config_dir = "./test_input-1_mappings-2_grids-192x94_to_1440x1080/";
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0) {
+
+        tango_init(config_dir.c_str(), "atm", 0, src_x, 0, src_y,
+                                              0, src_x, 0, src_y);
+        tango_begin_transfer(0, "ice");
+        tango_put("u", src_u, 192*94);
+        tango_end_transfer();
+
+    } else {
+        int x = 1440, y = 1080;
+        double u[x * y];
+
+        tango_init(config_dir.c_str(), "ice", 0, x, 0, y, 0, x, 0, y);
+        tango_begin_transfer(0, "atm");
+        tango_get("u", u, x*y);
+        tango_end_transfer();
+
+        /* Check that send and receive are the same. */
+        /*
+        for (int i = 0; i < l_rows * l_cols; i++) {
+            EXPECT_EQ(send_sst[i], recv_sst[i]);
+        }
+        */
+    }
+
+    tango_finalize();
+}
+
+
 /* Do single field send/receive between grids of different sizes. */
 TEST(Tango, send_receive_different_grids)
 {
