7231bf26b3aef341e4cc327ee0c62f3ad7ebf8f9
Nicholas Hannah <nicjhannah@gmail.com>
2017-03-20 11:06:39 +1100

    Setup performance test code to run on multiple procs.

--- test/CW2017/atm_perf.cc
+++ test/CW2017/atm_perf.cc
@@ -3,33 +3,38 @@
 #include <vector>
 #include "tango.h"
 
-#define NUM_FIELDS 1
-#define NUM_TIMESTEPS 100
+#define NUM_FIELDS 10
+#define NUM_TIMESTEPS 10
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-    int g_rows = 1080, g_cols = 1440, l_rows = 1080, l_cols = 1440;
+    int g_rows = 94, g_cols = 192, l_rows = 94, l_cols = 192;
     vector<double *> fields;
 
-    for(int f = 0; f < NUM_FIELDS; f++) {
+    for (int f = 0; f < NUM_FIELDS; f++) {
         fields.push_back(new double[l_rows * l_cols]);
     }
 
     MPI_Init(&argc, &argv);
-    tango_init("./", "ice", 0, l_rows, 0, l_cols, 0, g_rows, 0, g_cols);
+
+    const clock_t begin_time = clock();
+    tango_init("./", "atm", 0, l_rows, 0, l_cols, 0, g_rows, 0, g_cols);
 
     for (size_t t = 0; t < NUM_TIMESTEPS; t++) {
-        tango_begin_transfer("timestamp", "atm");
+        cout << "Timestep: " << t << endl;
+        tango_begin_transfer("timestamp", "ice");
 
         for (size_t f = 0; f < NUM_FIELDS; f++) {
-            tango_get("T_10", fields[f], l_rows * l_cols);
+            tango_put("T_10", fields[f], l_rows * l_cols);
         }
 
         tango_end_transfer();
     }
     tango_finalize();
+    cout << float( clock () - begin_time ) /  CLOCKS_PER_SEC << "s" << endl;
+
     MPI_Finalize();
 
     for (int f = 0; f < NUM_FIELDS; f++) {
--- test/CW2017/ice_perf.cc
+++ test/CW2017/ice_perf.cc
@@ -2,30 +2,82 @@
 #include <mpi.h>
 #include <vector>
 #include "tango.h"
+#include <assert.h>
 
-#define NUM_FIELDS 1
-#define NUM_TIMESTEPS 100
+#define NUM_FIELDS 10
+#define NUM_TIMESTEPS 10
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-    int g_rows = 94, g_cols = 192, l_rows = 94, l_cols = 192;
+    int g_rows = 1080, g_cols = 1440;
     vector<double *> fields;
 
-    for (int f = 0; f < NUM_FIELDS; f++) {
-        fields.push_back(new double[l_rows * l_cols]);
-    }
+    int rank, size;
+    int my_size, my_rank, my_root, my_row, my_col;
+    int i_pes, j_pes, i_per_pe, j_per_pe;
+    int lis, lie, ljs, lje;
 
     MPI_Init(&argc, &argv);
-    tango_init("./", "atm", 0, l_rows, 0, l_cols, 0, g_rows, 0, g_cols);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    /* Provide the number of PEs and root PE as an argument */
+    assert(argc == 3);
+    my_size = atoi(argv[1]);
+    my_root = atoi(argv[2]);
+    my_rank = rank - my_root;
+
+    if (my_size == 1) {
+        i_pes = 1;
+        j_pes = 1;
+    } else if (my_size == 2) {
+        i_pes = 2;
+        j_pes = 1;
+    } else if (my_size == 4) {
+        i_pes = 2;
+        j_pes = 2;
+    } else if (my_size == 480) {
+        i_pes = 24;
+        j_pes = 20;
+    } else if (my_size == 960) {
+        i_pes = 32;
+        j_pes = 30;
+    } else {
+        assert(false);
+    }
+
+    my_row = int(my_rank / i_pes);
+    my_col = my_rank % j_pes;
+
+    i_per_pe = int(g_cols / i_pes);
+    j_per_pe = int(g_rows / j_pes);
+
+    lis = my_col*i_per_pe;
+    lie = lis + i_per_pe;
+    ljs = my_row*j_per_pe;
+    lje = ljs + j_per_pe;
+
+    cout << "my_rank : " << my_rank << endl;
+    cout << "my_row : " << my_row << endl;
+    cout << "my_col : " << my_col << endl;
+    cout << "lis: " << lis << endl;
+    cout << "lie: " << lie << endl;
+    cout << "ljs: " << ljs << endl;
+    cout << "lje: " << lje << endl;
+
+    for (int f = 0; f < NUM_FIELDS; f++) {
+        fields.push_back(new double[i_per_pe * j_per_pe]);
+    }
+
+    tango_init("./", "ice", lis, lie, ljs, lje, 0, g_rows, 0, g_cols);
 
     for (size_t t = 0; t < NUM_TIMESTEPS; t++) {
-        cout << "Timestep: " << t << endl;
-        tango_begin_transfer("timestamp", "ice");
+        tango_begin_transfer("timestamp", "atm");
 
         for (size_t f = 0; f < NUM_FIELDS; f++) {
-            tango_put("T_10", fields[f], l_rows * l_cols);
+            tango_get("T_10", fields[f], i_per_pe * j_per_pe);
         }
 
         tango_end_transfer();
